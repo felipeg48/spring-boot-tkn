77baf8bf1cf6d853db9947c365294dc6565e4284
Felipe Gutierrez <felipeg@ibm.com>
2020-09-22 13:05:36 -0400

    updated

---
+++ spring-boot-tkn/src/main/java/com/ibm/demos/SpringBootTknApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.demos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTknApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTknApplication.class, args);
+	}
+
+}
---
+++ spring-boot-tkn/src/test/java/com/ibm/demos/SpringBootTknApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.demos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTknApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
